0b61d2f5483396edb82b64a0f7cb9f35e261285e
splach-coder <anasbenabbou776@gmail.com>
2024-03-10 01:01:24 +0100

    finsih the animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import "./Sass/main.scss";
 
-const App = () => {
-  return <div>App</div>;
-};
+// Components
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+import Loader from "./components/Loader";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    loading
+      ? document.querySelector("body").classList.add("loading")
+      : document.querySelector("body").classList.remove("loading");
+  }, [loading]);
+
+  return (
+    <AnimatePresence>
+      {loading ? (
+        <motion.div key="loader">
+          <Loader setLoading={setLoading} />
+        </motion.div>
+      ) : (
+        <div className="w-screen">
+          <Header />
+          <Banner />
+          {!loading && (
+            <div className="transition-image final">
+              <motion.img
+                transition={{ ease: [0.6, 0.01, -0.05, 0.9], duration: 1.6 }}
+                src={import.meta.env.BASE_URL + `images/image-2.jpg`}
+                layoutId="main-image-1"
+              />
+            </div>
+          )}
+        </div>
+      )}
+    </AnimatePresence>
+  );
+}
 
 export default App;
